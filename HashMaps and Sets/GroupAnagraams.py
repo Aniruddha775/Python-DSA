14e654254c52be17f004f4fd383228096144ac82
Aniruddha775 <aniruddhadchowdhury79@gmail.com>
2024-09-11 21:59:55 +0530

    Eleventh

---
+++ HashMaps and Sets/GroupAnagraams.py
@@ -0,0 +1,16 @@
+from typing import List
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_dict=defaultdict(list)
+        for s in strs:
+            count=[0]*26
+            for c in s:
+                count[ord(c)-ord('a')]+=1
+            key=tuple(count)
+            anagram_dict[key].append(s)
+        
+        return anagram_dict.values()
+    
+n=Solution()
+print(n.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))  
