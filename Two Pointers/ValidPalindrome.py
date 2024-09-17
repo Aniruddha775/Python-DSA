3654c5c9b0df42af8401b30739c8175467486b91
Aniruddha <aniruddhadc.dev@gmail.com>
2024-09-17 18:33:25 +0530

    ValidPalindrome

---
+++ Two Pointers/ValidPalindrome.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left:int=0
+        right:int=len(s)-1
+
+        while left<right:
+            if not s[left].isalnum():
+                left+=1
+                continue
+            if not s[right].isalnum():
+                right-=1
+                continue
+            if s[left].lower()!=s[right].lower():
+                return False
+            
+            left+=1
+            right-=1
+        
+        return True
+
+n=Solution()
+print(n.isPalindrome("A man, a plan, a canal: Panama"))
