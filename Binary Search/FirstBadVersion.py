48d402f8e053f0746c51c02e5864d559aad9a33e
Aniruddha <aniruddhadc.dev@gmail.com>
2024-09-23 20:54:03 +0530

    ValidPerfectSqaure

--- Binary Search/FirstBadVersion.py
+++ Binary Search/FirstBadVersion.py
@@ -2,10 +2,10 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         l=0
         r=n
-        while l<r:
-            m=l+(r-l)//2
-            if isBadVersion(m):
-                r=m
-            else:
-                l=m+1
+        # while l<r:
+        #     m=l+(r-l)//2
+        #     if isBadVersion(m):
+        #         r=m
+        #     else:
+        #         l=m+1
         return l # we can return r also
