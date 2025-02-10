0dbc6667b3a7e86be4222958185ba468346afdf3
Aniruddha775 <aniruddhadc.dev@gmail.com>
2025-02-10 19:58:05 +0530

    StackUsingQueue

---
+++ Heaps/StackUsingQueue.py
@@ -0,0 +1,18 @@
+from collections import deque
+class MyStack:
+
+    def __init__(self):
+        self.q=deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+
+    def pop(self) -> int:
+        for i in range(len(self.q)-1): #exclude the last element to pop it
+            self.push(self.q.popleft())
+        return self.q.popleft()
+    def top(self) -> int:
+        return self.q[-1]
+
+    def empty(self) -> bool:
+        return len(self.q)==0
